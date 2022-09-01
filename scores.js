491728b224b5363cb552b74a60545e8aa95054ef
CoderGirl <angi.adema@gmail.com>
2022-09-01 12:49:46 -0600

    created html for scores and .js for scores, added quiz questions and some info on the main html page.

--- scores.js
+++ scores.js
@@ -2,3 +2,4 @@
 
 // Event listener to clear the score.
 // For loop to recored all the scores. 
+// Button to go back and event listener to go back.
--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-//Quiz questions.
+//Quiz questions. Question: Options: Answer:
+var myQuestions = [
+    {
+        question: "Which HTML tag is self closing?",
+        options: ["<p>", "<a>", "<link>", "<h1>"],
+        answer: "<link>"        
+    },
+    {
+        question: "CSS targets the HTML for styling via: ?",
+        options: ["classes", "ids", "links", "all of the above"],
+        answer: "all of the above"        
+    },
+    {
+        question: "In JavaScript, when a variable is declared within a function, its scope is considered: ?",
+        options: ["declared", "global", "local", "equal"],
+        answer: "local"        
+    },
+    {
+        question: "In JavaScript, which method is used to round down to the next whole number?",
+        options: ["Math.random", "Math.round", "Math.floor", "Math.number"],
+        answer: "Math.floor"        
+    },
+];
 //Event listener for the timer.
+//Have to specify timer details from start, any penalties and when time is up.
+//Have to do a for loop to render the questions and selections to the page.
+//For loop to compare the questions with the answers.
