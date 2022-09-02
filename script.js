ecae41873f5a12ae3085d00b0a59216e2dc062c4
CoderGirl <angi.adema@gmail.com>
2022-09-02 09:43:07 -0600

    added html and pseudocode to js file.

--- script.js
+++ script.js
@@ -1,27 +1,79 @@
+var timerEl = document.getElementById('time');
+var startContainer = document.getElementById('start-container');
+var startBtn = document.getElementById('start');
+var questionContainer = document.getElementById('question-container');
+var finalScoreContainer = document.getElementById('final-score-container');
+var finalScoreEl = document.getElementById('finalScore');
+var initialsEl = document.getElementById('initials');
+var submitBtn = document.getElementById('submit');
+
+var questionArrayIndex = 0;
+var time = 40;
+var timerId;
+
 //Quiz questions. Question: Options: Answer:
 var myQuestions = [
     {
         question: "Which HTML tag is self closing?",
         options: ["<p>", "<a>", "<link>", "<h1>"],
-        answer: "<link>"        
+        answer: "<link>"
     },
     {
         question: "CSS targets the HTML for styling via: ?",
         options: ["classes", "ids", "links", "all of the above"],
-        answer: "all of the above"        
+        answer: "all of the above"
     },
     {
         question: "In JavaScript, when a variable is declared within a function, its scope is considered: ?",
         options: ["declared", "global", "local", "equal"],
-        answer: "local"        
+        answer: "local"
     },
     {
         question: "In JavaScript, which method is used to round down to the next whole number?",
         options: ["Math.random", "Math.round", "Math.floor", "Math.number"],
-        answer: "Math.floor"        
+        answer: "Math.floor"
     },
 ];
-//Event listener for the timer.
+
+
+
 //Have to specify timer details from start, any penalties and when time is up.
 //Have to do a for loop to render the questions and selections to the page.
 //For loop to compare the questions with the answers.
+
+function start() {
+    // hide the start container and display the question container
+
+    startContainer.classList.add = 'hidden'
+    questionContainer.classList.remove = 'hidden';
+
+    // start the timer
+
+
+    // run a function that will show your questions
+    displayQuestion()
+}
+
+function displayQuestion() {
+    // need to create the elements that will go in the question container
+
+    //need to add the content from the current question
+
+    //need to create the container to house the button choices.
+
+    //need to loop the array of choices
+
+    //need to creat the buttons and add the content and event listenter  to the buttons
+
+    //need to append the buttons to the button container
+
+
+    //need to append all create element to the question container. 
+}
+
+// create function that will tie to the button event listener. to check the answer clicked.
+
+
+
+//Event listener to start the quiz.
+startBtn.addEventListener('click', start)
